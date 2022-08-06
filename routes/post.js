c1795e27a61af64c6798c9926bf20be40a2f6da4
Vinshavijayan <vinshakv00@gmail.com>
2022-08-07 01:28:28 +0400

    post  autherization completed

--- routes/post.js
+++ routes/post.js
@@ -1,15 +1,16 @@
 const express = require("express")
+const auth = require("../middleware/auth");
 const { addPostData,getAllPostData,getPostDatabyID,getPostDatabyIDandUpdate,getPostDatabyIdandDelete } = require("../controllers/post")
 
 const post = require("../models/post")
 const router = express.Router()
 
 
-router.post('/',addPostData)
+router.post('/',auth,addPostData)
 router.get('/all',getAllPostData)
-router.get('/:id',getPostDatabyID)
-router.put('/:id',getPostDatabyIDandUpdate)
-router.delete('/:id',getPostDatabyIdandDelete)
+router.get('/:id',auth,getPostDatabyID)
+router.put('/:id',auth,getPostDatabyIDandUpdate)
+router.delete('/:id',auth,getPostDatabyIdandDelete)
 
 
 
